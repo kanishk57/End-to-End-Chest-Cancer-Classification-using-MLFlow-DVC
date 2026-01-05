e98ce3b1ab7fb684b1b93fe9bd4e8661a37ed940
kanishk57 <dhimankanishk21@gmail.com>
2026-01-05 17:32:14 +0000

    installed Dependencies

---
+++ documentation.md
@@ -0,0 +1,10 @@
+what broke, why broke, how it was fixed. 
+
+1,
+
+a,  ![alt text](image-1.png)
+    could not download TF with/without a specified version.
+b,  there was multiple layers of errors, checked proxy, configs, firewall protection, tried upgrading pip, reinstalling python, changed multiple venvs. 
+c, eventually when nothing worked i installed all dependencies using Ubuntu WSL because it bypass all the proxy configurations. ![alt text](image-2.png)
+
+2, 
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-End-Chest-Cancer-Classification-using-MLFlow-DVC"
+AUTHOR_USER_NAME = "kanishk57"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "kdhiman.personal@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small project to classify chest cancer using MLFlow and DVC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_url={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
